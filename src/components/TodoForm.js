834fa1cda7b5c645dd8738df07a21782516dcd51
RatniChaima <ratnichayma@gmail.com>
2022-09-05 14:08:55 +0200

    Creating Components

---
+++ src/components/TodoForm.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import{v4 as uuidv4} from 'uuid';
+
+function TodoForm(props) {
+    const [input, setInput]= useState('');
+
+    const handleChange = e => { setInput(e.target.value); } ;
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // props.onSubmit({
+        //     id: uuidv4(),
+        //     text:input
+        // })
+        setInput('');
+
+    };
+     
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+        <input
+        type="text"
+        placeholder="Enter a Todo"
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handleChange}
+        />
+        <button className="todo-button">Add </button>
+
+    </form>
+  )
+}
+
+export default TodoForm
